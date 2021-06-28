ae663d5290f57d2af22cf24551ed019bb42e3be2
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2021-06-27 17:37:31 -0700

    Fixed regression model to not parse floats and accept floats by default

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const csv = require('csvtojson');
+const mlreg = require('ml-regression');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -9,24 +10,24 @@ const app = express();
 const datasetInfo = [
     {
         id: 0,
-        name: "Automobile MPG Data",
+        name: "Automobile MPG Dataset",
         type: "Regression",
         entires: 398,
         description: "This dataset contains a list of 398 automobiles as well as some information about each. You will attempt to predict the MPG an automobile recieves."
     },
     {
         id: 1,
-        name: "Automobile MPG Data",
-        type: "Regression",
-        entires: 398,
-        description: "This dataset contains a list of 398 automobiles as well as some information about each. You will attempt to predict the MPG an automobile recieves."
+        name: "Iris Flower Dataset",
+        type: "Classification",
+        entires: 150,
+        description: "This dataset contains a list of characterists pertaining to 150 individual iris flowers. There are 3 classes of such flowers and you will attempt to predict which class a given flower falls under."
     },
     {
         id: 2,
-        name: "Automobile MPG Data",
-        type: "Regression",
-        entires: 398,
-        description: "This dataset contains a list of 398 automobiles as well as some information about each. You will attempt to predict the MPG an automobile recieves."
+        name: "Census Income Dataset",
+        type: "Classification",
+        entires: 48842,
+        description: "This dataset contains data from the 1994 US Census pertaining to nearly 49,000 people. Your task is to determine whether or not a given person makes over $50,000 a year."
     }
 ];
 
@@ -36,6 +37,7 @@ const datasetPaths = [
     '/datasets/auto-mpg/auto-mpg.txt'
 ]
 
+app.use(express.json());
 // Routes
 app.get('/api/datasets/info/:id', (req, res) => {
     res.json(datasetInfo[req.params.id]);
@@ -47,8 +49,14 @@ app.get('/api/datasets/:id', (req, res) => {
     });
 })
 
-app.get('/api/train', (req, res) => {
-
+app.post('/api/predict', (req, res) => {
+    // Create model
+    let regressionModel = new mlreg.MultivariateLinearRegression(req.body.train.X, req.body.train.y);
+    let preds = []
+    req.body.test.X.forEach((row) => {
+        preds.push(regressionModel.predict(row));
+    })
+    res.json({real: req.body.test.y, predictions: preds});
 });
 
 app.get("/api", (req, res) => {
